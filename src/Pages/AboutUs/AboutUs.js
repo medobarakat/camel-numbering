2c90ff52201637c7eab1888ece4517025f62030f
medobarakat <ahmedbarakat2401@gmail.com>
2023-08-02 13:29:36 +0300

    finish about us

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Logo from "../../Assets/Images/Anaam.png"
 // styles
 import "./AboutUs.scss"
 import { Container } from '@mui/material'
@@ -15,10 +16,36 @@ const AboutUs = () => {
                 </Container>
             </div>
             <Container>
-                <div>
-                    1
+                <div className='firstContent'>
+                    <div className='imgContainer'>
+                        <img width={300} src={Logo} alt='logo'/>
+                    </div>
+                    <div className='textWrapper'>
+                        <p>
+                        إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على وجه الخصوصهذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة
+                        </p>
+                    </div>
                 </div>
-
+                <div className='lastContent'>
+                    <div className='Lasttitle'>
+                        <p>
+                            اهدافنا
+                        </p>
+                    </div>
+                    <div className='aboutUsSteps'>
+                        <ul>
+                            <li>
+                            هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة
+                            </li>
+                            <li>
+                            يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق
+                            </li>
+                            <li>
+                            إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على وجه الخصوص
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </Container>
         </div>
     )
--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,66 +1,110 @@
-import React from 'react'
+import React from 'react';
 // logo
-import ContactUsLogo from "../../Assets/Logo/ContactUsLogo"
+import ContactUsLogo from '../../Assets/Logo/ContactUsLogo';
 // styles
-import "./ContactUs.scss"
-import { Button, Container, TextField } from '@mui/material'
+import './ContactUs.scss';
+import { Button, Container, TextField } from '@mui/material';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  fullName: Yup.string().required('الاسم بالكامل مطلوب'),
+  email: Yup.string().email('البريد الإلكتروني غير صالح').required('البريد الإلكتروني مطلوب'),
+  phoneNumber: Yup.string().required('رقم الجوال مطلوب'),
+  message: Yup.string().required('رسالتك مطلوبة'),
+});
 
 const ContactUs = () => {
-    return (
-        <div className='ContactUsWrapper'>
-            <div className='title'>
-                <Container>
-                    <h5>
-                        تواصل معنا
-                    </h5>
-                </Container>
-            </div>
-            <Container>
-
-                <div className='lowerContent'>
-
-                    <div className='rightContent'>
-                        <div className='inputWithText'>
-                            <span>الاسم بالكامل</span>
-                            <TextField id="outlined-basic" fullWidth variant="outlined" />
-                        </div>
-                        <div className='twoInputContainer'>
-                           
-                            <div className='inputWithText'>
-                                <span>البريد الالكترونى</span>
-                                <TextField id="outlined-basic" fullWidth variant="outlined" />
-                            </div>
-                            <div className='inputWithText'>
-                                <span>رقم الجوال</span>
-                                <TextField id="outlined-basic" fullWidth variant="outlined" />
-                            </div>
-                        </div>
-                        <div className='inputWithText'>
-                            <span>رسالتك</span>
-                            <TextField
-                                fullWidth
-                                multiline
-                                sx={{
-                                    width: "100%",
-                                    background:"#FAFAF"
-                                }}
-                            />
-
-                        </div>
-                        <Button sx={{
-                            borderRadius: 3,
-                            background: "#5DBB67",
-                            fontSize: 16,
-                            width: "320px", fontFamily: "inherit", marginTop: 4
-                        }} variant="contained" color="success">ارسال</Button>
+  return (
+    <div className='ContactUsWrapper'>
+      <div className='title'>
+        <Container>
+          <h5>تواصل معنا</h5>
+        </Container>
+      </div>
+      <Container>
+        <Formik
+          initialValues={{
+            fullName: '',
+            email: '',
+            phoneNumber: '',
+            message: '',
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            // Handle form submission logic here
+            console.log(values);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className='lowerContent'>
+                <div className='rightContent'>
+                  <div className='inputWithText'>
+                    <span>الاسم بالكامل</span>
+                    <Field name='fullName' as={TextField} fullWidth variant='outlined' />
+                    {errors.fullName && touched.fullName ? (
+                      <div className='error'>{errors.fullName}</div>
+                    ) : null}
+                  </div>
+                  <div className='twoInputContainer'>
+                    <div className='inputWithText'>
+                      <span>البريد الالكترونى</span>
+                      <Field name='email' as={TextField} fullWidth variant='outlined' />
+                      {errors.email && touched.email ? (
+                        <div className='error'>{errors.email}</div>
+                      ) : null}
                     </div>
-                    <div className='LeftContent'>
-                        <ContactUsLogo />
+                    <div className='inputWithText'>
+                      <span>رقم الجوال</span>
+                      <Field name='phoneNumber' as={TextField} fullWidth variant='outlined' />
+                      {errors.phoneNumber && touched.phoneNumber ? (
+                        <div className='error'>{errors.phoneNumber}</div>
+                      ) : null}
                     </div>
+                  </div>
+                  <div className='inputWithText'>
+                    <span>رسالتك</span>
+                    <Field
+                      name='message'
+                      as={TextField}
+                      fullWidth
+                      multiline
+                      sx={{
+                        width: '100%',
+                        background: '#FAFAF',
+                      }}
+                    />
+                    {errors.message && touched.message ? (
+                      <div className='error'>{errors.message}</div>
+                    ) : null}
+                  </div>
+                  <Button
+                    type='submit'
+                    sx={{
+                      borderRadius: 3,
+                      background: '#5DBB67',
+                      fontSize: 16,
+                      width: '320px',
+                      fontFamily: 'inherit',
+                      marginTop: 4,
+                    }}
+                    variant='contained'
+                    color='success'
+                  >
+                    ارسال
+                  </Button>
                 </div>
-            </Container>
-        </div>
-    )
-}
+                <div className='LeftContent'>
+                  <ContactUsLogo />
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
