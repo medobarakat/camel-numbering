d0fd754c882f371d84807c733bf5153f005ea9ed
medobarakat <ahmedbarakat2401@gmail.com>
2023-08-08 10:21:42 +0300

    adding redux

---
+++ src/Features/store.js
@@ -0,0 +1,10 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './user/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
